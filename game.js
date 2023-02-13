0ae9aab80497a1d2d489f1786425514af35b80d5
creativoo <creativoo@gmail.com>
2023-02-13 14:01:09 -0700

    agregando mapas y dandoles funcion

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log (maps);
+const canvas = document.querySelector("#game");
+const game = canvas.getContext ("2d");
+
+window.addEventListener("load", startGame);
+
+function startGame () {
+    // game.fillRect(0,50,100,100); // De que punto partirá el cuadrado y cuanto medirá (x=0, y=0, tamaño x=100, y=100)
+    // game.clearRect (50,50,50,50); // Sirve para borrar el rectángulo.
+    // game.clearRect ()
+
+    game.font = "25px Verdana";
+    game.fillStyle = "purple";
+    game.textAlign = "center"; // inicia o termina el texto en la acción aplicada si aplicas "End" terminará en el fillText indicado (25,25).
+    game.fillText("Platzi", 25,25);
+}
